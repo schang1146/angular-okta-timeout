ca478dc4a8ca0dd9e6304732ba155ac7119aa0cb
schang1146 <20074034+schang1146@users.noreply.github.com>
2024-04-19 13:35:58 -0700

    add okta callback route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 
 const routes: Routes = [
+  {
+    path: 'callback',
+    component: OktaCallbackComponent,
+  },
   {
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
+    canActivate: [OktaAuthGuard],
   },
   {
     path: '**',
