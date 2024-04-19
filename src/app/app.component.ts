0f6100c8291af4349afb97f7ca204b6581225dcc
schang1146 <20074034+schang1146@users.noreply.github.com>
2024-04-19 13:36:26 -0700

    add logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-okta-timeout-test';
+  constructor(@Inject(OKTA_AUTH) public oktaAuth: OktaAuth) {}
+
+  logout() {
+    this.oktaAuth.signOut();
+  }
 }
