1f9fa6346b7002f51e3b2c7ba4572e9c5c38e53a
schang1146 <20074034+schang1146@users.noreply.github.com>
2024-04-19 14:02:10 -0700

    keep and update auth state to show in UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component, Inject } from '@angular/core';
-import { OKTA_AUTH } from '@okta/okta-angular';
-import { OktaAuth } from '@okta/okta-auth-js';
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH, OktaAuthStateService } from '@okta/okta-angular';
+import {
+  AccessToken,
+  IDToken,
+  OktaAuth,
+  RefreshToken,
+} from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(@Inject(OKTA_AUTH) public oktaAuth: OktaAuth) {}
+export class AppComponent implements OnInit {
+  isAuthenticated: boolean | undefined;
+  accessToken: AccessToken | undefined;
+  idToken: IDToken | undefined;
+  refreshToken: RefreshToken | undefined;
+  error: Error | undefined;
+
+  constructor(
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth,
+    private oktaAuthStateService: OktaAuthStateService
+  ) {}
+
+  ngOnInit(): void {
+    this.oktaAuthStateService.authState$.subscribe((authState) => {
+      this.isAuthenticated = authState.isAuthenticated;
+      this.accessToken = authState.accessToken;
+      this.idToken = authState.idToken;
+      this.refreshToken = authState.refreshToken;
+      this.error = authState.error;
+    });
+  }
 
   logout() {
     this.oktaAuth.signOut();
